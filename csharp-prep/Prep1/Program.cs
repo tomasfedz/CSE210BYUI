50d999addf92618910f0f26f5eaa26cefd9ea49b
Tomas <your.email@example.com>
2025-01-11 21:26:45 -0700

    W01 hw

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+      // Prompt user for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt user for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display full name
+        Console.WriteLine($"Hello, {firstName} {lastName}. My name is Tom Riddle");
+
+        // Pause program before closing
+        Console.ReadKey();
     }
 }
