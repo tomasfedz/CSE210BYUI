12e6ab3743d9db0fa84f8cdd92a9f9f8d9c1e06e
Tomas <your.email@example.com>
2025-01-11 21:52:39 -0700

    grade calculator submission

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,68 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask user for grade percentage
+        Console.Write("What is your grade percentage: ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        // Determine letter grade
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine sign (+/-) for letter grade
+        string sign = "";
+        if (letter != "F" && letter != "A")
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && grade < 93)
+        {
+            sign = "-";
+        }
+
+        // Print letter grade and sign 
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        // Determine if user passed the course or not
+        if (grade >= 70)
+        {
+            Console.WriteLine("Good for you, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Aw man, Better luck next time :/");
+        }
+
+        // Pause program before closing
+        Console.ReadKey();
     }
 }
